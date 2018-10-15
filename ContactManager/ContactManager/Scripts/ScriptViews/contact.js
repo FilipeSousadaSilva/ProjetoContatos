0cf36540db84b1a8054c2afaad5857566cc39fc6
filipess <filipess.2011@gmail.com>
2018-10-15 02:44:02 -0300

    Separando código js do html

---
+++ ContactManager/ContactManager/Scripts/ScriptViews/contact.js
@@ -0,0 +1,20 @@
+﻿$(function()
+    {
+        $.getJSON('/api/contact', function(contactsJsonPayload)
+        {
+            $(contactsJsonPayload).each(function(i, item)
+            {
+                $('#contacts').append('<li>' + item.Name + '</li>');
+            });
+        });
+    });
+    
+$('#saveContact').click(function () {
+    $.post("/api/contact",
+          $("#saveContactForm").serialize(),
+          function (value) {
+              $('#contacts').append('<li>' + value.Name + '</li>');
+          },
+          "json"
+    );
+});
