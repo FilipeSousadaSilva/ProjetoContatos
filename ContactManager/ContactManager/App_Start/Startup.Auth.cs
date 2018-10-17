78040f817815d7a3c834a7b67f08be016c2c7483
filipess <filipess.2011@gmail.com>
2018-10-17 16:43:55 -0300

    Removendo código desnecessário

--- ContactManager/ContactManager/App_Start/Startup.Auth.cs
+++ ContactManager/ContactManager/App_Start/Startup.Auth.cs
@@ -8,17 +8,13 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using ContactManager.Providers;
 using ContactManager.Models;
 
 namespace ContactManager
 {
     public partial class Startup
     {
-        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
-
-        public static string PublicClientId { get; private set; }
-
+       
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
